66afa95e2ea2a3bcde5fbad27a81a136aeb827f3
ShapneelAsn <shap.ahsan@gmail.com>
2025-04-14 22:16:06 -0400

    deploy to GH Page

--- src/App.js
+++ src/App.js
@@ -138,7 +138,7 @@ class App extends Component {
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
-      <Router basename="/bank-of-react-starter-code">
+      <Router basename="/assignment_3_bank_of_react_v2">
         <div>
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
